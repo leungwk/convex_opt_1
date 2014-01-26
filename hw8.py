b9b45537a9241223d57f6388df3db206a76a49a3
William Leung <wleung@wlcomp>
2014-01-26 15:30:56 -0500

    Added LP solver using the barrier method (a kind of interior-point algorithm) given a strictly feasible starting point.
    Renamed functions to reflect problem they were solving.

--- hw8.py
+++ hw8.py
@@ -1,9 +1,11 @@
 import numpy as np
 from numpy.random import RandomState
-import os
 import pandas as pd
 
-from lp import run_newton, _plot_newton_convergence, _f, _grad
+from .lp import run_lp_center, _plot_lp_center, _f, _grad, run_lp_strict, _plot_lp_strict
+
+import cvxopt
+import cvxpy
 
 image_dir = 'img/'
 
@@ -35,17 +37,14 @@ c = prng.rand(n,1)
 
 
 ## part 1
-df_newton_stats, df_op = run_newton(A, b, c, x0)
-image_path = image_dir +'lp_part_1_-_newton_convergence.png'
-_plot_newton_convergence(df_newton_stats, df_op, image_path)
+acc_df_lp_center_stats, df_op = run_lp_center(A, b, c, x0)
+image_path = image_dir +'lp_-_part_1_-_centering.png'
+_plot_lp_center(acc_df_lp_center_stats, df_op, image_path)
 
 
 
 
 ## compare against cvxpy
-import cvxopt
-import cvxpy
-
 x = cvxpy.Variable(n,name='x')
 A1 = cvxopt.matrix(A)
 c1 = cvxopt.matrix(c)
@@ -84,4 +83,38 @@ for _, row in df_op.iterrows():
     entry = (in_domain, p_f_eq, st_norm, p_star, cvx_p_star, cvx_st_norm)
     acc_stats.append(entry)
 df_stats = pd.DataFrame(acc_stats, columns=['dom','pf_eq','st_norm','p_star','cvx_p_star','cvx_st_norm'])
+print "Part 1: Centering (comparison against cvxpy)"
+print df_stats
+
+
+
+## part 2
+def orig_f(c,x): # the "f" in lp.py includes the barrier; this is the original objective
+    return np.dot(c.T,x)
+acc_df_lp_strict_stats, df_op_lp_strict = run_lp_strict(A, b, c, x0.copy()) # use default starting values
+image_path = image_dir +'lp_-_part_2_-_lp_strict_start.png'
+_plot_lp_strict(acc_df_lp_strict_stats, df_op_lp_strict, image_path)
+
+## compare against cvxpy
+x = cvxpy.Variable(n,name='x')
+A1 = cvxopt.matrix(A)
+c1 = cvxopt.matrix(c)
+ones = cvxopt.matrix(np.ones(n))
+b1 = cvxpy.Variable(b.shape[0],name='b')
+objective = cvxpy.Minimize(
+    c1.T*x
+)
+constraints = [A1*x == b1, x >= 0]
+prob = cvxpy.Problem(objective,constraints)
+p_star_1 = prob.solve()
+
+
+acc_stats = []
+for _, row in df_op_lp_strict.iterrows():
+    x_star, nu_star, mu = row[['x_star','nu_star','mu']]
+    p_star_diff = orig_f(c, x_star) -orig_f(c,x.value)
+    row = (p_star_diff, mu)
+    acc_stats.append( row )
+df_stats = pd.DataFrame(acc_stats, columns=['p_star_diff','mu'])
+print "Part 2: LP with strictly feasible starting point (comparison against cvxpy)"
 print df_stats
--- lp.py
+++ lp.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 import itertools
-import functools
 
 """
 Solve a standard form LP using the barrier method
 """
 
 def _f(c,x):
+    """Note: For part 2, t*c will give the log barrier approximation of the LP. The original objective is c^Tx"""
     return np.dot(c.T,x) -np.sum(np.log(x))
 
 
@@ -48,7 +48,7 @@ def line_search(c, x, grad_x, delta_x_nt, alpha, beta):
     return t
 
 
-def newton(A, b, c, x, epsilon=1e-6, max_iter=100, alpha=0.25, beta=0.5):
+def lp_center(A, b, c, x, epsilon=1e-6, max_iter=100, alpha=0.25, beta=0.5):
     acc_stats = []
     for iter_n in xrange(max_iter):
         ## solve kkt system
@@ -81,18 +81,18 @@ def newton(A, b, c, x, epsilon=1e-6, max_iter=100, alpha=0.25, beta=0.5):
     return x, w, df
 
 
-def run_newton(A, b, c, x0):
-    df_stats = []
+def run_lp_center(A, b, c, x0):
+    acc_df_stats = []
     ops_params = []
     for alpha, beta in itertools.product([0.01,0.1,0.3],[0.1,0.5,0.8]):
-        x_star, nu_star, df_stat = newton(A, b, c, x0.copy(), alpha=alpha, beta=beta)
-        df_stats.append(df_stat)
+        x_star, nu_star, df_stat = lp_center(A, b, c, x0.copy(), alpha=alpha, beta=beta)
+        acc_df_stats.append(df_stat)
         ops_params.append((x_star, nu_star, alpha, beta, _f(c, x_star)))
     df_op = pd.DataFrame(ops_params, columns=['x_star', 'nu_star', 'alpha', 'beta', 'f'])
-    return df_stats, df_op
+    return acc_df_stats, df_op
 
 
-def _plot_newton_convergence(dfs, df_op, image_path):
+def _plot_lp_center(dfs, df_op, image_path):
     plt.close()
     fig, ax = plt.subplots(nrows=1, ncols=1, sharex=True)
     rainbow = ax._get_lines.color_cycle
@@ -104,8 +104,54 @@ def _plot_newton_convergence(dfs, df_op, image_path):
     ax.set_yscale('log')
     ax.set_xlabel('k')
     ax.set_ylabel(r'$\lambda_2/2$')
-    ax.set_title('Convergence of Newton\'s method for LP')
+    ax.set_title('Newton\'s method for centering step using LP')
     ax.legend(title=r'$\alpha,\beta$',prop={'size':10},loc='upper right')
     plt.savefig(image_path,bbox_inches='tight',pad_inches=0)
     print "Wrote to {0}".format(image_path)
     # plt.show()
+
+
+def lp_strict(A, b, c, x, t=1, mu=10, epsilon=1e-3):
+    """Assuming a strictly feasible starting point"""
+    n = len(x)
+    acc_stats = []
+    while True:
+        x_star, nu_star, df_stat = lp_center(A, b, c*t, x, alpha=0.01, beta=0.5) # c*t will create objective tc^Tx -1^Tlog(x), the original objective plus log barrier
+        x = x_star
+        gap = 1.*n/t
+        row = (len(df_stat), gap)
+        acc_stats.append( row ) # num Newton steps per centering
+        if gap < epsilon:
+            break
+        t *= mu
+    df = pd.DataFrame(acc_stats, columns=['k_newton','gap'])
+    return x_star, nu_star, df
+
+
+def run_lp_strict(A, b, c, x0):
+    acc_df_stats = []
+    op_params = []
+    for mu in [2,10,50,200]:
+        x_star_strict, nu_star_strict, df_stats_strict = lp_strict(A, b, c, x0.copy(), mu=mu)
+        acc_df_stats.append( df_stats_strict )
+        op_params.append( (x_star_strict, nu_star_strict, mu) )
+    df_op = pd.DataFrame(op_params, columns=['x_star','nu_star','mu'])
+    return acc_df_stats, df_op
+
+
+def _plot_lp_strict(dfs, df_op, image_path):
+    plt.close()
+    fig, ax = plt.subplots(nrows=1, ncols=1, sharex=True)
+    rainbow = ax._get_lines.color_cycle
+    markers = itertools.cycle(['o',">","v","<","^","s","+","x","D"])
+    line_cycle = itertools.cycle(["-","-.","--",":",])
+    for df, mu, c, marker, ls in zip(dfs, df_op['mu'], rainbow, markers, line_cycle):
+        label = str(mu)
+        ax.step(df['k_newton'].cumsum(), df['gap'], color=c, label=label, linestyle=ls, markersize=4)
+    ax.set_yscale('log')
+    ax.set_xlabel('Newton iterations')
+    ax.set_ylabel('duality gap')
+    ax.set_title('Effect of step size in LP using barrier method')
+    ax.legend(title='mu',prop={'size':10},loc='upper right')
+    plt.savefig(image_path,bbox_inches='tight',pad_inches=0)
+    print "Wrote to {0}".format(image_path)
